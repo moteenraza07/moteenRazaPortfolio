e7aff6b65b64ec33c023a51a46ed314819734a05
Moteen Raza <moteenraza@gmail.com>
2022-12-15 13:55:13 -0500

    completed section three and Four

---
+++ script.js
@@ -0,0 +1,19 @@
+// function skillShow () {
+//     let show = document.querySelectorAll(".skillShow");
+//     for (let i = 0; i < show.length; i++) {
+//         let windowHeight = window.innerHeight;
+//         let elementTop = show[i].getBoundingClientRect().top;
+//         let elementVisible = 100;
+//         if (elementTop < windowHeight - elementVisible) {
+//           show[i].classList.add("active");
+//         } else {
+//           show[i].classList.remove("active");
+//         }
+//       }
+//     }
+
+//     window.addEventListener("scroll", skillShow);
+
+//     skillShow()
+console.log("Hello WORLD")
+AOS.init();
