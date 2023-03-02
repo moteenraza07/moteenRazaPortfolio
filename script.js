7c43b7dfc00c6ac2d119a1233acaadc140c066a6
Moteen Raza <moteenraza@gmail.com>
2023-03-02 17:00:11 -0500

    completed media query for the portfolio down to 320px

--- script.js
+++ script.js
@@ -15,5 +15,52 @@
 //     window.addEventListener("scroll", skillShow);
 
 //     skillShow()
-console.log("Hello WORLD")
-AOS.init();
+
+// create empty object for name spacing
+const app = {};
+
+// target the mobile button class
+app.button = document.querySelector('.mobileButton');
+
+// target the mobile menu class
+app.mobileMenu = document.querySelector('.mobileMenu');
+
+// target the navContainer class
+app.navContainer = document.querySelector('.navContainer');
+
+// target the closeMenu icon
+app.close = document.querySelector('.closeMenuIcon');
+
+// listen for a click event on the hamburger menu button
+// The click on the button will add target certain class and remove others. 
+app.button.addEventListener("click", function() {
+    // toggle on and off
+    // grab the mobile menu and when i click it display the menu
+
+    app.button.classList.toggle('open');
+    app.navContainer.classList.remove('hiddenItems');
+    app.close.classList.remove('closeMenuIcon');
+    app.close.classList.add('closeMenu');
+
+
+})
+
+
+app.close.addEventListener("click", function() {
+    // toggle on and off
+    // grab the mobile menu and when i click it display the menu
+
+    app.button.classList.toggle('open');
+    app.navContainer.classList.add('hiddenItems');
+    app.close.classList.remove('closeMenuIcon');
+    app.close.classList.add('closeMenu');
+
+
+})
+
+
+
+
+
+
+
